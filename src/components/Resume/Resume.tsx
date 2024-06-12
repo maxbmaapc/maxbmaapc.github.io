ad780bc11e3e8f1d681a1818def89a328f4d9e64
Maksim Lukianenko <maxbuzz8694@gmail.com>
2024-06-12 20:41:59 +0100

    chore(index.html): update page title and add meta tags for description and og:image to improve SEO and social media sharing feat(photo.png): add photo.png to project for use in og:image meta tag fix(Resume.tsx): update link to resume to use correct Google Cloud Storage URL

--- src/components/Resume/Resume.tsx
+++ src/components/Resume/Resume.tsx
@@ -5,7 +5,7 @@ import Motion from "../Motion/Motion";
 const Resume = () => {
   return (
     <Motion className="resume" viewport={0.1}>
-      <a href="https://storage.cloud.google.com/maksim_lukianneko_cv/Maksim%20Lukianenko_CV.pdf">
+      <a href="https://storage.googleapis.com/maksim_lukianneko_cv/Maksim%20Lukianenko_CV.pdf">
         View My Resume
         <TfiDownload className="resume__arrow_icon" />
       </a>
